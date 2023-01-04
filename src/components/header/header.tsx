93ebcd9984b613c102f07ff813ff14ea8ed03688
Dávid Molnár <mdavid626@gmail.com>
2023-01-04 17:53:29 +0100

    Drop console.log

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -12,7 +12,6 @@ const links = [
 
 const Header: React.FC = () => {
   const location = useLocation();
-  console.log(location.pathname);
   return (
     <div className="Header">
       <div className="Header-title">Fides Sample App</div>
