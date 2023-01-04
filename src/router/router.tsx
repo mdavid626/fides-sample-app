4d873512e3c59b17e5a29a90389ede1cb58c3c42
Dávid Molnár <mdavid626@gmail.com>
2023-01-04 18:17:38 +0100

    Add test error page

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -4,12 +4,17 @@ import MoviesPage from '../pages/movies-page/movies-page';
 import AboutPage from '../pages/about-page/about-page';
 import FavouritesPage from '../pages/favourites-page/favourites-page';
 
+const ErrorPage: React.FC = () => {
+  throw new Error('test error');
+};
+
 const Router: React.FC = () => (
   <HashRouter>
     <Routes>
       <Route path="/" element={<MoviesPage />} />
       <Route path="/favourites" element={<FavouritesPage />} />
       <Route path="/about" element={<AboutPage />} />
+      <Route path="/error" element={<ErrorPage />} />
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   </HashRouter>
