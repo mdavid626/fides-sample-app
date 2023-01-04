653126fb0b7cd85b63f7fce9336fc3e8ea0e27ce
Dávid Molnár <mdavid626@gmail.com>
2023-01-04 15:23:54 +0100

    Select current page

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,17 +1,36 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import classnames from 'classnames';
+import { Link, useLocation } from 'react-router-dom';
 
 import './header.css';
 
-const Header: React.FC = () => (
-  <div className="Header">
-    <div className="Header-title">Fides Sample App</div>
-    <div className="Header-navigation">
-      <Link to="/">Movies</Link>
-      <Link to="/favourites">Favourites</Link>
-      <Link to="/about">About</Link>
+const links = [
+  { path: '/', title: 'Movies' },
+  { path: '/favourites', title: 'Favourites' },
+  { path: '/about', title: 'About' },
+];
+
+const Header: React.FC = () => {
+  const location = useLocation();
+  console.log(location.pathname);
+  return (
+    <div className="Header">
+      <div className="Header-title">Fides Sample App</div>
+      <div className="Header-navigation">
+        {links.map((link) => (
+          <Link
+            key={link.path}
+            to={link.path}
+            className={classnames('Header-link', {
+              'Header-selectedLink': link.path === location.pathname,
+            })}
+          >
+            {link.title}
+          </Link>
+        ))}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Header;
