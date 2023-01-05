54d3877a3dd4f5785e259f5891b7dc224e5a7ae3
Dávid Molnár <mdavid626@gmail.com>
2023-01-05 12:10:30 +0100

    Add tests for movies-hooks

---
+++ src/hooks/movies-hooks/movies-hooks.spec.ts
@@ -0,0 +1,131 @@
+import { cleanup, waitFor } from '@testing-library/react';
+import moviesService from '../../services/movies-service';
+import { renderHookWithQueryClient } from '../../testing-library/render';
+import { movie1, movies } from '../../test-data/movies';
+import {
+  useMovies,
+  useFavourites,
+  useAddToFavourites,
+  useRemoveFromFavourites,
+} from './movies-hooks';
+
+jest.mock('../../services/movies-service');
+
+describe('movies-hooks', () => {
+  beforeEach(() => {
+    jest.spyOn(console, 'error').mockReturnValue(undefined);
+    jest.spyOn(window, 'alert').mockReturnValue(undefined);
+  });
+  afterEach(cleanup);
+  afterEach(jest.resetAllMocks);
+
+  describe('useMovies', () => {
+    it('should load movies', async () => {
+      (moviesService.getMovies as jest.Mock).mockResolvedValue(movies);
+      const { result } = renderHookWithQueryClient(() => useMovies(1));
+      expect(result.current).toEqual([undefined, true, null]);
+      await waitFor(() => {
+        expect(result.current).toEqual([movies, false, null]);
+      });
+      expect(moviesService.getMovies).toHaveBeenCalledWith(1);
+    });
+
+    it('should return error when error', async () => {
+      (moviesService.getMovies as jest.Mock).mockRejectedValue(
+        new Error('test error')
+      );
+      const { result } = renderHookWithQueryClient(() => useMovies(1));
+      expect(result.current).toEqual([undefined, true, null]);
+      await waitFor(() => {
+        expect(result.current).toEqual([
+          undefined,
+          false,
+          new Error('test error'),
+        ]);
+      });
+    });
+  });
+
+  describe('useFavourites', () => {
+    it('should load favourites', async () => {
+      (moviesService.getFavourites as jest.Mock).mockResolvedValue(movies);
+      const { result } = renderHookWithQueryClient(() => useFavourites());
+      expect(result.current).toEqual([undefined, true, null]);
+      await waitFor(() => {
+        expect(result.current).toEqual([movies, false, null]);
+      });
+      expect(moviesService.getFavourites).toHaveBeenCalledWith();
+    });
+
+    it('should return error when error', async () => {
+      (moviesService.getFavourites as jest.Mock).mockRejectedValue(
+        new Error('test error')
+      );
+      const { result } = renderHookWithQueryClient(() => useFavourites());
+      expect(result.current).toEqual([undefined, true, null]);
+      await waitFor(() => {
+        expect(result.current).toEqual([
+          undefined,
+          false,
+          new Error('test error'),
+        ]);
+      });
+    });
+  });
+
+  describe('useAddToFavourites', () => {
+    it('should add to favourites', async () => {
+      (moviesService.addToFavourites as jest.Mock).mockResolvedValue(movies);
+      const { result, queryClient } = renderHookWithQueryClient(() =>
+        useAddToFavourites()
+      );
+      expect(result.current).toEqual([expect.any(Function), false]);
+      result.current[0](movie1);
+      await waitFor(() => {
+        expect(moviesService.addToFavourites).toHaveBeenCalledWith(movie1);
+      });
+      expect(queryClient.getQueryData('favourites')).toEqual(movies);
+    });
+
+    it('should inform user when error', async () => {
+      (moviesService.addToFavourites as jest.Mock).mockRejectedValue(
+        new Error('test error')
+      );
+      const { result } = renderHookWithQueryClient(() => useAddToFavourites());
+      result.current[0](movie1);
+      await waitFor(() => {
+        expect(window.alert).toHaveBeenCalledWith('test error');
+      });
+    });
+  });
+
+  describe('useRemoveFromFavourites', () => {
+    it('should remove from favourites', async () => {
+      (moviesService.removeFromFavourites as jest.Mock).mockResolvedValue(
+        movies
+      );
+      const { result, queryClient } = renderHookWithQueryClient(() =>
+        useRemoveFromFavourites()
+      );
+      expect(result.current).toEqual([expect.any(Function), false]);
+      result.current[0](movie1);
+      await waitFor(() => {
+        expect(moviesService.removeFromFavourites).toHaveBeenCalledWith(movie1);
+      });
+      expect(queryClient.getQueryData('favourites')).toEqual(movies);
+    });
+
+    it('should inform user when error', async () => {
+      (moviesService.removeFromFavourites as jest.Mock).mockRejectedValue(
+        new Error('test error')
+      );
+      const { result } = renderHookWithQueryClient(() =>
+        useRemoveFromFavourites()
+      );
+      result.current[0](movie1);
+      await waitFor(() => {
+        expect(window.alert).toHaveBeenCalledWith('test error');
+      });
+    });
+  });
+});
--- src/testing-library/render.tsx
+++ src/testing-library/render.tsx
@@ -1,8 +1,18 @@
 import React, { ReactElement } from 'react';
 import { createMemoryRouter, RouterProvider } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import {
+  render,
+  renderHook,
+  RenderHookOptions,
+  RenderOptions,
+} from '@testing-library/react';
+import { queries, Queries } from '@testing-library/dom';
 
-export const renderWithRouter = (component: ReactElement) => {
+export const renderWithRouter = (
+  component: ReactElement,
+  options?: Omit<RenderOptions, 'queries'>
+) => {
   const router = {} as { current?: ReturnType<typeof createMemoryRouter> };
   return {
     ...render(component, {
@@ -16,7 +26,42 @@ export const renderWithRouter = (component: ReactElement) => {
         router.current = memoryRouter;
         return <RouterProvider router={memoryRouter} />;
       },
+      ...options,
     }),
     router,
+  };
+};
+
+export const renderHookWithQueryClient = <
+  Result,
+  Props,
+  Q extends Queries = typeof queries,
+  Container extends Element | DocumentFragment = HTMLElement,
+  BaseElement extends Element | DocumentFragment = Container
+>(
+  render: (initialProps: Props) => Result,
+  options?: RenderHookOptions<Props, Q, Container, BaseElement>
+) => {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnReconnect: false,
+        refetchOnWindowFocus: false,
+        refetchOnMount: false,
+        retry: false,
+        staleTime: 0,
+      },
+    },
+  });
+  return {
+    ...renderHook(render, {
+      wrapper: ({ children }) => (
+        <QueryClientProvider client={queryClient}>
+          {children}
+        </QueryClientProvider>
+      ),
+      ...options,
+    }),
+    queryClient,
   };
 };
