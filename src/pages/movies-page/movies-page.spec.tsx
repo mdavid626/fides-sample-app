a355adb6acdb477dc862dcd29893f0fea98311e6
Dávid Molnár <mdavid626@gmail.com>
2023-01-05 11:41:40 +0100

    Add even more tests for movies page

--- src/pages/movies-page/movies-page.spec.tsx
+++ src/pages/movies-page/movies-page.spec.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { cleanup, render } from '@testing-library/react';
+import { cleanup, render, screen } from '@testing-library/react';
 import Header from '../../components/header/header';
 import Footer from '../../components/footer/footer';
 import MovieItem from '../../components/movie-item/movie-item';
+import Pagination from '../../components/pagination/pagination';
 import {
   useFavourites,
   useMovies,
@@ -11,7 +12,6 @@ import { usePagination } from '../../hooks/pagination-hooks/pagination-hooks';
 import { movie1 } from '../../test-data/movies';
 import { moviesResponse1 } from '../../test-data/movies-response';
 import MoviesPage from './movies-page';
-import Pagination from '../../components/pagination/pagination';
 
 jest.mock('../../components/header/header');
 jest.mock('../../components/footer/footer');
@@ -79,5 +79,71 @@ describe('movies-page', () => {
     expect(useMovies).toHaveBeenCalledWith(1);
     expect(useFavourites).toHaveBeenCalledWith();
     expect(usePagination).toHaveBeenCalledWith();
+  });
+
+  it('should render spinner when loading movies', () => {
+    (useMovies as jest.Mock).mockReturnValue([undefined, true, null]);
+    render(<MoviesPage />);
+    expect(screen.getByTestId('PageLoader-spinner')).toBeVisible();
+    expect(screen.getByText('header')).toBeVisible();
+    expect(screen.getByText('footer')).toBeVisible();
+    expect(screen.queryByText('pagination')).toBe(null);
+    expect(screen.queryByText('movie-item')).toBe(null);
+    expect(Header).toHaveBeenCalledWith({}, {});
+    expect(Footer).toHaveBeenCalledWith({}, {});
+    expect(Pagination).not.toHaveBeenCalled();
+    expect(MovieItem).not.toHaveBeenCalled();
+  });
+
+  it('should render spinner when loading favourites', () => {
+    (useFavourites as jest.Mock).mockReturnValue([undefined, true, null]);
+    render(<MoviesPage />);
+    expect(screen.getByTestId('PageLoader-spinner')).toBeVisible();
+    expect(screen.getByText('header')).toBeVisible();
+    expect(screen.getByText('footer')).toBeVisible();
+    expect(screen.queryByText('pagination')).toBe(null);
+    expect(screen.queryByText('movie-item')).toBe(null);
+    expect(Header).toHaveBeenCalledWith({}, {});
+    expect(Footer).toHaveBeenCalledWith({}, {});
+    expect(Pagination).not.toHaveBeenCalled();
+    expect(MovieItem).not.toHaveBeenCalled();
+  });
+
+  it('should render error when loading movies error', () => {
+    (useMovies as jest.Mock).mockReturnValue([
+      undefined,
+      false,
+      new Error('test error'),
+    ]);
+    render(<MoviesPage />);
+    expect(screen.getByText('test error')).toBeVisible();
+    expect(screen.queryByText('PageLoader-spinner')).toBe(null);
+    expect(screen.getByText('header')).toBeVisible();
+    expect(screen.getByText('footer')).toBeVisible();
+    expect(screen.queryByText('pagination')).toBe(null);
+    expect(screen.queryByText('movie-item')).toBe(null);
+    expect(Header).toHaveBeenCalledWith({}, {});
+    expect(Footer).toHaveBeenCalledWith({}, {});
+    expect(Pagination).not.toHaveBeenCalled();
+    expect(MovieItem).not.toHaveBeenCalled();
+  });
+
+  it('should render error when loading favourites error', () => {
+    (useFavourites as jest.Mock).mockReturnValue([
+      undefined,
+      false,
+      new Error('test error'),
+    ]);
+    render(<MoviesPage />);
+    expect(screen.getByText('test error')).toBeVisible();
+    expect(screen.queryByText('PageLoader-spinner')).toBe(null);
+    expect(screen.getByText('header')).toBeVisible();
+    expect(screen.getByText('footer')).toBeVisible();
+    expect(screen.queryByText('pagination')).toBe(null);
+    expect(screen.queryByText('movie-item')).toBe(null);
+    expect(Header).toHaveBeenCalledWith({}, {});
+    expect(Footer).toHaveBeenCalledWith({}, {});
+    expect(Pagination).not.toHaveBeenCalled();
+    expect(MovieItem).not.toHaveBeenCalled();
   });
 });
